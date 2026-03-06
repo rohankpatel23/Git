e3ea06afb734befcec045ed351da4815262bc337
Rohan Patel <rohan.k.patel23@gmail.com>
2026-03-05 17:05:41 -0800

    Added Camera Functionality

--- idk.py
+++ idk.py
@@ -4,3 +4,18 @@ import tensorflow as tf
 
 import tensorflow_datasets as tfds
 
+
+#Added camera functionality via openCV library
+#Close video by pressing 'q' key
+import cv2
+
+cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+while True:
+    ret, img = cam.read()
+    cv2.imshow("Live Camera", img)
+    key = cv2.waitKey(1) & 0Xff
+    if key == ord('t'):
+        cam.release()
+        break
+
